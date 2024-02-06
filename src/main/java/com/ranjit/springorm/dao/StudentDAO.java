f22037e9e832334a298cd3543cc854659362cccf
ranjitsutar <ranjitsutar456@gmail.com>
2024-02-06 20:15:02 +0530

    CURD operation

--- src/main/java/com/ranjit/springorm/dao/StudentDAO.java
+++ src/main/java/com/ranjit/springorm/dao/StudentDAO.java
@@ -1,26 +1,68 @@
 package com.ranjit.springorm.dao;
 
 
+import java.util.List;
+
+
 import org.springframework.orm.hibernate5.HibernateTemplate;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.ranjit.springorm.entites.Student;
 
+import jakarta.transaction.Transactional;
+
+
 public class StudentDAO {
 
-	@Transactional
 	private HibernateTemplate hibernateTemplate;
 	
+	@Transactional
+	// Insart 
 	public int insert(Student student) {
 		
 		Integer res = (Integer)this.hibernateTemplate.save(student);
 		return res;
 	}
-
+	
+	
+	
+	// get the single data(object)
+	
+	public Student getStudentById(int studentId) {
+		
+		Student student = this.hibernateTemplate.get(Student.class, studentId);
+		return student;
+		
+	}
+	
+	public List<Student> getAllStudent(){
+		
+		List<Student> allStudent = this.hibernateTemplate.loadAll(Student.class);
+		return allStudent;
+		
+	}
+	// get All Student
 	public HibernateTemplate getHibernateTemplate() {
 		return hibernateTemplate;
 	}
-
+	
+	@Transactional
+	// Delete Student By Id
+	public Student deletStudent(int studentId) {
+		Student student = this.hibernateTemplate.get(Student.class, studentId);
+		this.hibernateTemplate.delete(student);
+		return student;
+	}
+	
+	@Transactional
+	//	Update Student By Id
+	public void updateStudentById(Student student) {
+		this.hibernateTemplate.update(student);
+		
+	}
+	
+	
+	
+	
 	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
 		this.hibernateTemplate = hibernateTemplate;
 	}
